7d88c950577319acb8575e4e9b7bff5fd234c742
ZabuzaW <zabuza.dev@gmail.com>
2017-03-17 20:13:59 +0100

    Script now accounts for inexistent entries
    If an entry does not exist it builds it from scratch and if it exists it
    does only replace the entry.
    The script now also supports infinitely many fastspells.

--- src/FakeFastspell_-_Freewar/FakeFastspell_-_Freewar.user.js
+++ src/FakeFastspell_-_Freewar/FakeFastspell_-_Freewar.user.js
@@ -17,40 +17,86 @@ function isSponsor() {
 }
 
 /*
- * Processes a given fastspell and exchanges the link with the given data.
+ * Checks whether the fastspell menu is present or not. For example it is not present if the inventory is opened.
+ * @returns True if the fastspell menu is present, false if not
+ */
+function isFastspellMenuPresent() {
+	return $('table#fastspell').length > 0;
+}
+
+/*
+ * Processes a given fastspell and exchanges the link with the given data or builds an entry for the fastspell if not present.
  * @param fastspellId The id of the fastspell to exchange
  * @param itemId The id of the item to activate with this fastspell
  * @param itemName The name of the item to activate with this fastspell
  */
 function processFastspell(fastspellId, itemId, itemName) {
-	var anchor = $('a#accessfast' + fastspellId);
-	var fastItemAmount = $(anchor).next('span.fastitemamount');
-	
-	// Abort if element is not present, for example if inventory is opened
-	if ($(anchor).length <= 0) {
+	// Abort if fastspell id is not valid
+	if (fastspellId <= 0) {
 		return;
 	}
 	
-	// Remove the fast item amount if present
-	if ($(fastItemAmount).length > 0) {
-		$(fastItemAmount).remove();
+	var anchor = $('a#accessfast' + fastspellId);
+	
+	// If fastspell has an entry, exchange the link
+	if ($(anchor).length > 0) {
+		var fastItemAmount = $(anchor).next('span.fastitemamount');
+		
+		// Remove the fast item amount if present
+		if ($(fastItemAmount).length > 0) {
+			$(fastItemAmount).remove();
+		}
+	
+		// Exchange link
+		$(anchor).attr('href', 'item.php?action=activate&act_item_id=' + itemId);
+		$(anchor).attr('onclick', 'this.href += \'&yscroll=\' + window.pageYOffset;');
+		$(anchor).text(itemName);
+	
+		// Remove the id to prevent an easy detection of the script
+		$(anchor).removeAttr('id');
+	} else {
+		// If fastspell has no entry, build it
+		
+		// Fetch an anchor point where to append the new entry
+		var anchorPoint;
+		if (fastspellId > 1) {
+			// Use the previous spell as anchor point
+			anchorPoint = $('table#fastspell' + (fastspellId - 1));
+		} else {
+			// Use the menu header as anchor point
+			anchorPoint = $('table#fastspell');
+		}
+		
+		// If anchor is not present, abort
+		if ($(anchorPoint).length <= 0) {
+			return;
+		}
+		
+		var contentToAdd = '\
+			<table id="fastspell' + fastspellId + '" width="100%" cellspacing="0" cellpadding="0">\
+				<tbody>\
+					<tr>\
+						<td width="95%">\
+							Nr. <b>' + fastspellId + '</b>: <a href="item.php?action=activate&act_item_id=' + itemId
+								+ '" onclick="this.href += \'&yscroll=\' + window.pageYOffset;">' + itemName + '</a>\
+						</td>\
+						<td valign="top">\
+							<img src="../images/close.gif" border="0">\
+						</td>\
+					</tr>\
+				</tbody>\
+			</table>';
+		
+		$(anchorPoint).after(contentToAdd);
 	}
-	
-	// Exchange link
-	$(anchor).attr('href', 'item.php?action=activate&act_item_id=' + itemId);
-	$(anchor).attr('onclick', 'this.href += \'&yscroll=\' + window.pageYOffset;');
-	$(anchor).text(itemName);
-	
-	// Remove the id to prevent an easy detection of the script
-	$(anchor).removeAttr('id');
 }
 
 /*
  * Routine function of the script.
  */
 function routine() {
-	// Abort if user is sponsor
-	if (isSponsor()) {
+	// Abort if user is sponsor or fastspell menu is not present, for example if inventory is opened
+	if (isSponsor() || (!isFastspellMenuPresent())) {
 		return;
 	}
 	
